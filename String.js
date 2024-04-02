fcbf0f0fd3a61b1dbaf936d1e33140ceb6abc4e4
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-02 22:55:35 +0530

    Update String.js

--- String.js
+++ String.js
@@ -1,1 +1,48 @@
 
+
+function getLength(str){
+  console.log("String length", str.length)
+}
+
+function findIndexOf(str,target){
+  consoe.log("Index:", str.indexOf(target))
+}
+
+
+function LastIndexOf(str,target){
+  consoe.log("Index:", str.lastindexOf(target))
+}
+
+
+function getSlice(str, start, end) {
+  console.log("Original String:", str);
+  console.log("After slice:", str.slice(start, end));
+}
+
+function getSubstring(str, start, end) {
+  console.log("Original String:", str);
+  console.log("After substring:", str.substring(start, end));
+}
+
+function splitString(str, separator) {
+  console.log("Original String:", str);
+  console.log("After split:", str.split(separator));
+}
+
+
+function trimString(str) {
+  console.log("Original String:", str);
+  console.log("After trim:", str.trim());
+}
+
+
+function toUpper(str) {
+  console.log("Original String:", str);
+  console.log("After toUpperCase:", str.toUpperCase());
+}
+
+function toLower(str) {
+  console.log("Original String:", str);
+  console.log("After toLowerCase:", str.toLowerCase());
+}
+
