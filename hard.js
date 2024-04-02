b4acf22006722ca54549d446a0e171e21aab040f
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-02 21:45:15 +0530

    Update hard.js

--- hard.js
+++ hard.js
@@ -1,1 +1,15 @@
+// Write a function for debouncing
 
+function debounce(func , delay) {
+  let timer;
+  return function(){
+    clearTimeout()
+    setTimeout(func,delay)
+  }
+}
+
+function callback(){
+  console.log("I will be called after delay")
+}
+
+console.log(debounce(callback,5000))
