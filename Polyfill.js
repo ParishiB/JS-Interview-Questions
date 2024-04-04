192b55beb82589235ae74a1441835b39dceaf68b
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-04 22:59:19 +0530

    Update Polyfill.js

--- Polyfill.js
+++ Polyfill.js
@@ -6,3 +6,29 @@ Array.prototype.myForEach = function(cb) {
     cb(this[i])
   }
 }
+
+
+// Array Map
+
+Array.prototype.myMap = function (cb) {
+  const res = [];
+  for (let i = 0; i < this.length; i++) {
+    res.push(cb(this[i]));
+  }
+  return res;
+};
+
+
+// Array Filter
+
+Array.prototype.myFilter = function(cb) {
+  const res = []
+  for (let i = 0; i < this.length; i++) {
+     if(cb(this[i])){
+      res.push(this[i])
+     }
+  }
+  return res;
+}
+
+
