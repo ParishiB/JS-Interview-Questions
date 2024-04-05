7e6081bf06314a99c6478b83a9dd36636e576748
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-05 23:40:51 +0530

    Create Node_js_basic.js

---
+++ Node_js_basic.js
@@ -0,0 +1,17 @@
+Q1) What is node.js?
+ sol - The nodejs is runtime enviornment for Javascript
+
+Q2) How Node is a runtime environment on server side ?  what is V8 ?
+
+Q3) What is difference between runtime environment and framework ? 
+  sol - Runtime enviornment primarly focuses on providing the necessary infrastructure for code execution , including services like memory management andI/O operation
+   Framework primarly focuses on simplifying the development process by offering a structured set of tools, libraries and best practices
+
+Q4) What is difference between node.js and express.js ?
+express.js is a framework built on the top of node.js.It is designed to simplify the process of building web applications and APIS and providing a set of fetaures like simple routing system, middleware support etc.
+
+Q5) What are the differences between client side and server side ? 
+
+
+
+
