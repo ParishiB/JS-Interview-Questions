751013d744a8ff9789b951afdede379c158d56fc
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-06 00:29:51 +0530

    Update Main_features_node.js

--- Main_features_node.js
+++ Main_features_node.js
@@ -1,1 +1,39 @@
+Q) What are 7 main features of node.js ?
+
+• Single threaded
+• Asynchronous
+• Event-driven
+• V8 Javascript engine
+• Cross-Platform
+• NPM
+• Real-Time Capabilites
+
+
+Q) What is Single Threaded Programming?
+  A single-threaded language is one that can execute only one task at a time. The program will execute the tasks in sequence, and each task must complete before the next task starts.
+
+Q) What is Synchronous Programming?
+  One task id performed only after one if performed, synchronous is blocking approach
+  
+Q) What is multi threaded prgramming?
+  It is good for CPU intensive process
+
+Q) What is asynchronous prograaming?
+  we know synchronous is blocking approach
+
+
+
+Q) What are events, event emitter , event queue , event loop and event driven?
+Q) What are main features and the advantages of nodejs?
+  •asynchronus - handle multiple concurrent requests and non blocking execution of thread
+  •V8 JS engine - excutes code very fast
+  •Event-driven architecture - handling events will be very easy
+  •cross-platofrm - supports deployment on various operating systems, enhancing flexibility
+  •javascript
+
+Q) What are disadvantages of nodejs?
+   Avoid in CPU intensive tasks, multi threaded will be btter compared to 
+ 
+  
+
 
