13b13777da4a2c41925f7e36de6150b3f9525693
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-06 12:40:26 +0530

    Update node_built_modules.js

--- node_built_modules.js
+++ node_built_modules.js
@@ -1,1 +1,33 @@
+Q) Top 5 built in modules?
+  1)fs  2) path  3) os 4)events 5)http
+
+## fs modules
+
+const fs = require("fs")
+
+fs.writeFileSync('./test.tsxt' , "Hey there")
+// the above function will synchronously create the new file named test.txt and will wrute Hey there inside it
+// Sync means it is a synchronous call
+fs.writeFile('./test/txt' , "Hey there" ,(err) => console.log(err) return;)
+
+const res = fs.readFileSync('./contacts.txt' , 'utf-8')
+console.log(result)
+
+fs.readFileSync('./contacts.txt' , 'utf-8' , ((err,data) => {
+  if(err)console.log(err)
+  console.log(data)
+})
+
+// async will not return anything , nut sync will return 
+fs.appendFileSync("./test.txt" , `Date.now().toLocaleString`)
+
+
+## Q) Nodejs architecture
+## Synchronous and asynchronous
+
+nodejs -> event queue -> option 1 (blocking operation) || option 2(non-blocking operation) 
+if(non blocking AKA asynchronous) It will be immediately solved
+if(blocking AKA synchronous) It will be send to Thread pool and workers will be assigned to it
+
+
 
