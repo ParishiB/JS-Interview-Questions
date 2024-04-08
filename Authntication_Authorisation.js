7ad76336fc6e77b25162d3c40c3d566535d6b2d0
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-08 12:49:29 +0530

    Create Authntication_Authorisation.js

---
+++ Authntication_Authorisation.js
@@ -0,0 +1,22 @@
+Q) What are authntication and authorisation ?
+  authentication(who are you ?)
+  authorisation (different accesss to different roles is called authorisation)
+
+Q) Types of authntication in nodejs?
+  1) Basic
+  2) API Key authntication
+  3 ) multi factor authntication
+  4) certificate based authentication
+
+
+Q) What is the role of hashing and salt?
+
+  password -hasing(using algorithm)--->too add more security we genrate salt by using bcrypt library---->secure password will be salt  + hash
+
+Q) How can we create Hash passwords in nodejs ?
+
+Q) What is token based and JWT authentication ?
+  
+
+
+  
