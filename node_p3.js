126207ac2b87c351e5928ad8fb738ab17fc06b66
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-04-08 12:32:57 +0530

    Create node_p3.js

---
+++ node_p3.js
@@ -0,0 +1,27 @@
+  Q) what is cors?
+   CORS or Cross-Origin Resource Sharing in Node. js is a mechanism by which a front-end client can make requests for resources to an external back-end server. The single-origin policy does not allow cross-origin requests and CORS headers are required to bypass this feature
+
+ Q) What is serialisation and deserilisation ?
+
+  // why do we need to send it in json not object?
+ // json is common for every technology and api
+
+   serialization  is the process of converting an object into a format that can be stored , transmitted , or reconstructed later
+   Deserialization is the process of converting serialized data such as binary/XML/JSON back into an object
+
+Q) What are types of serilaization?
+  Binary
+  XML
+  JSON
+
+Q) How to serialise and deserialise in nodejs?
+  JSON.stringify
+  JSON.parse
+
+Q) Explain the concept of versioning in RESTful APIs
+  we are creating to versions v1, v2, v3  
+
+Q) What is typical structure of a REST API project in Node?
+   MVC
+  
+   
