84a561c1e35e04a9df18f70249376280ad4693a5
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-07-04 21:33:33 +0530

    Create Intv.js

---
+++ Intv.js
@@ -0,0 +1,10 @@
+Intv questions
+1) Rem vs em
+2) Box model
+3) box-sizing :border-box;�box-sizing: content-box;
+4) stacking context
+5) useMemo and usecallback
+6) useRef
+7) IIFE ? Where is it used
+8) Output based JS question
+9) Machine coding round - Todo variation
