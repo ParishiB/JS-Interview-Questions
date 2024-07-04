69989ed60c2fd9c2491c740f181ea8eebd27e94f
Parishi <117592741+ParishiB@users.noreply.github.com>
2024-07-04 14:48:05 +0530

    Update form-with-json-vals.jsx

--- React/form-with-json-vals.jsx
+++ React/form-with-json-vals.jsx
@@ -1,1 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+const data = {
+  configs: {
+    type: "object",
+    properties: {
+      clientKey: {
+        type: "string",
+        title: "Client Id",
+      },
+      pspId: {
+        type: "string",
+        title: "BillDesk issued merchant id",
+        minLength: 1,
+      },
+    },
+  },
+};
+
+const App = () => {
+  const [vals, setVals] = useState([]);
+  const properties = data.configs.properties;
+
+  useEffect(() => {
+    const renderVals = (properties) => {
+      const newVals = [];
+      for (let key in properties) {
+        newVals.push(key);
+      }
+      setVals(newVals);
+    };
+
+    renderVals(properties);
+  }, [properties]);
+
+  return (
+    <div>
+      {vals.map((item, index) => (
+        <div key={index} className="">
+          {item}
+          <input type="text" placeholder="Enter values here" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
 
